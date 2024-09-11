a6411844f496a378c1439df8dc6e923b11ec19fe
jyoshnavaka <jyoshna.v3@gmil.com>
2024-09-11 11:59:14 +0530

    addDelete

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Cards, {  Count, Loader, Toast } from './index.jsx'
-import { Converter } from './class/classcomponent.jsx'
-import { FakeStoreData } from './class/fetchandAxios.jsx'
-import { AddDelete } from './class/AddandDelete.jsx'
-import { Bulb } from './class/buld.jsx'
-import { Form, Table } from 'react-bootstrap'
-import { CustomTable } from './class/table.jsx'
-import { Weather } from './class/weather.jsx'
-import { EvenOdd } from './functional/evenorodd.jsx'
-import { FormValidation } from './functional/formvalidation.jsx'
-import { DynamicTable } from './class/dynamictable.jsx'
-import { DisplayItems } from './class/display.jsx'
-import{TableForm} from './functional/formTable.jsx'
-import { TableFormUpdate } from './functional/formUpdate.jsx'
-import Navigation from './functional/navbar.jsx'
-import { TableNav } from './functional/tableSearch.jsx'
-import Reducer from './functional/reducer.jsx'
 
-// import List from './component/component'
-// import Table from './table'
+import './App.css'
+import { AddDelete } from './class/AddandDelete.jsx'
+
+import { FakeStoreData } from './class/fetchandAxios.jsx'
+
 function App() {
-  // click=(onpress)={
-  //  // return( <input type="tel" value={name}/>)
-  // }
+  
 
   return(
 
   
 <>
-{/* <Cards text='hello'>
-  <h4>Children</h4>
-  <Toast></Toast>
-  </Cards>
-<Count></Count>
-<Loader height={100} width={100}></Loader> */}
 
+ {/* <FakeStoreData></FakeStoreData> */}
+ <AddDelete/>
 
-{/* <Converter  ></Converter> */}
- <FakeStoreData></FakeStoreData>
- {/* <Bulb></Bulb> */}
- {/* <CustomTable></CustomTable> */}
-{/* <AddDelete></AddDelete> */}
-{/* <Weather></Weather> */}
-{/* <EvenOdd></EvenOdd> */}
-{/* <FormValidation></FormValidation> */}
-{/* <DynamicTable></DynamicTable> */}
-{/* <DisplayItems></DisplayItems> */}
-{/* <TableForm></TableForm> */}
-{/* <TableFormUpdate></TableFormUpdate> */}
-{/* <Navigation>
-</Navigation> */}
-{/* <TableNav></TableNav> */}
-{/* <Reducer></Reducer> */}
 
 </>
   )}
